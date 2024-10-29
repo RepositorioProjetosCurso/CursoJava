dc349094c93931d7f30bcd15815aa75bada2b76f
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-10-29 10:47:30 -0300

    Adicionando paciente e transformando em um objeto

--- 3. Orientacao a Objetos/CalculadoraIMC/src/Main.java
+++ 3. Orientacao a Objetos/CalculadoraIMC/src/Main.java
@@ -2,14 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         CalculadoraImc calculadoraImc1 = new CalculadoraImc();
+        Paciente p1 = new Paciente();
 
-        calculadoraImc1.altura = 1.75;
-        calculadoraImc1.peso = 98.80;
+        p1.altura = 1.52;
+        p1.peso = 124.73;
 
-        IndiceMassaCorporal imc = calculadoraImc1.calcular();
+        IndiceMassaCorporal imc = calculadoraImc1.calcular(p1);
 
         if(imc.estaComObesidade()) {
-            System.out.printf("CalculadoraImc com altura %.2f e peso %.2f, está com obesidade%n", imc.altura, imc.peso);
+            System.out.printf("Paciente com altura %.2f e peso %.2f, está com obesidade%n", imc.altura, imc.peso);
         }
 
         System.out.printf("IMC: %.2f", imc.resultado);
---
+++ 3. Orientacao a Objetos/CalculadoraIMC/src/Paciente.java
@@ -0,0 +1,8 @@
+public class Paciente {
+
+    String nome;
+    int idade;
+    double peso;
+    double altura;
+
+}
