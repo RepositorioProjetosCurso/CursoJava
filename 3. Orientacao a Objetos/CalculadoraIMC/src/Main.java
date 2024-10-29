53965d10c71ed59eec414250fbd16634245a6c8c
RepositorioCursos <repositoriocursosgit@gmail.com>
2024-10-29 09:27:37 -0300

    Atribuindo o nome e a responsabilidade da Classe

--- 3. Orientacao a Objetos/CalculadoraIMC/src/Main.java
+++ 3. Orientacao a Objetos/CalculadoraIMC/src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Paciente paciente1 = new Paciente();
+        CalculadoraImc calculadoraImc1 = new CalculadoraImc();
 
-        paciente1.altura = 1.75;
-        paciente1.peso = 98.80;
+        calculadoraImc1.altura = 1.75;
+        calculadoraImc1.peso = 98.80;
 
-        IndiceMassaCorporal imc = paciente1.calcularIndiceMassaCorporea();
+        IndiceMassaCorporal imc = calculadoraImc1.calcular();
+
+        if(imc.estaComObesidade()) {
+            System.out.printf("CalculadoraImc com altura %.2f e peso %.2f, está com obesidade%n", imc.altura, imc.peso);
+        }
+
         System.out.printf("IMC: %.2f", imc.resultado);
     }
 }
